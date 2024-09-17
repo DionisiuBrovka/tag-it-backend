3136260c0b12bb0c0ad198842e92c558883ee93d
sdsd <dev.dionisiu.brovka@gmail.com>
2024-09-17 13:21:57 +0300

    Добавлены базовые модели данных

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class AppUser(AbstractUser):
+  
+    profile_picture = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to="user_pics/",
+        verbose_name="Изображение профиля"
+    ) 
+
+    desc = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Описание профиля"
+    )
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
+class UserLinks(models.Model):
+
+    url = models.URLField(
+        null=False,
+        blank=False,
+        verbose_name="Ссылка"
+    )
+
+    user = models.ForeignKey(
+        AppUser, 
+        on_delete=models.CASCADE, 
+        null=False,
+        blank=False,
+        related_name="links", 
+        verbose_name="Пользователь"
+    )
+
+    class Meta:
+        verbose_name = "Ссылка"
+        verbose_name_plural = "Ссыллки"
